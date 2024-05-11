01e414985dc8ccf70af41c1b3741f9f04316b207
neutronahmed <bazubairu@st.ug.edu.gh>
2024-05-11 18:29:27 +0000

    transaction validations added

--- app/traits/StripeHelperTrait.php
+++ app/traits/StripeHelperTrait.php
@@ -33,4 +33,23 @@ trait StripeHelperTrait
         ]);
         return $payment_methods;
     }
+    public function useCard($customer_id, $amount)
+    {
+        $card = $this->getPaymentMethods($customer_id);
+
+        try {
+            \Stripe\PaymentIntent::create([
+                'amount' => $amount * 100,
+                'currency' => 'usd',
+                'customer' => $customer_id,
+                'payment_method' => $card->data[0]->id,
+                'off_session' => true,
+                'confirm' => true
+            ]);
+        } catch (\Stripe\Exception\CardException $e) {
+            return response()->back()->with([
+                'error' => 'Something went wrong. Error code: ' . $e->getMessage()
+            ]);
+        }
+    }
 }

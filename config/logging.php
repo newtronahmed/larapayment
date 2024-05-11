50b24bf0aa31b6791c014486254b91268cd20800
neutronahmed <bazubairu@st.ug.edu.gh>
2024-05-11 19:26:59 +0000

    deploying on heroku

--- config/logging.php
+++ config/logging.php
@@ -58,7 +58,7 @@ return [
         ],
 
         'single' => [
-            'driver' => 'single',
+            'driver' => env('App_ENV') == 'local' ? 'single': 'errorlog',
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
         ],
